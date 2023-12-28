038333940d71c5aca791b68411addcf6e2d75739
BH kang <rkdqudgjs1127@gmail.com>
2023-12-28 15:43:55 +0900

    [BE] 로그인 기능

--- src/main/java/com/example/hackaton/service/LoginService.java
+++ src/main/java/com/example/hackaton/service/LoginService.java
@@ -12,12 +12,6 @@ public class LoginService {
 
     private final UserRepository userRepository;
 
-    public User login(String loginId, String password) {
-        return userRepository.findByEmail(loginId)
-                .filter(m -> m.getPassword().equals(password))
-                .orElse(null);
-    }
-
 
 
 }
